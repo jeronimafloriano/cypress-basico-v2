56c436965bdc192202c238161947f621e833dd15
jeronima <jeronima.floriano@totvs.com.br>
2023-03-08 20:25:33 -0300

    Testes de requisição de API, invocação de elementos escondidos na página e alterando informações da página

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -221,9 +221,28 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .should('not.be.visible')
       })
 
-    it.only('preenche a area de texto usando o comando invoke', function() {
+    it('preenche a area de texto usando o comando invoke', function() {
         const longText = Cypress._.repeat('Teste ', 50)
         cy.get('#open-text-area').invoke('val' , longText).should('have.value', longText)
+    })
+
+    it('faz uma requisição HTTP', function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should((response) => {
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT');
+            })
+    })
+
+    it('Desafio: encontre o gato escondido', function() {
+        cy.get('#cat').invoke('show').should('be.visible')
+    })
+
+    it('alterando informações da página', function() {
+        cy.get('#title').invoke('text', 'CAT TAT')
+        cy.get('#subtitle').invoke('text', 'Praticando automação com Cypress!!!! ❤️❤️❤️')
     })
 
 })
